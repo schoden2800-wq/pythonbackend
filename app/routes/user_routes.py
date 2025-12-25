6ac9fc0b5601959af61de159db7dc828083c73a1
schoden2800-wq <schoden2800@gmail.com>
2025-12-25 13:40:23 +0600

    updated routes

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,3 +1,40 @@
+# # from fastapi import APIRouter
+# # from app.controller.user_controller import (
+# #     register_admin,
+# #     login,
+# #     forgot_password,
+# #     verify_otp,
+# #     reset_password
+# # )
+# # from app.schemas.user_schema import (
+# #     RegisterAdmin,
+# #     Login,
+# #     ForgotPassword,
+# #     VerifyOtp,
+# #     ResetPassword
+# # )
+
+# # router = APIRouter(prefix="/api")
+
+# # @router.post("/login")
+# # def login_route(data: Login):
+# #     return login(data)
+
+# # @router.post("/admin/register")
+# # def register_admin_route(data: RegisterAdmin):
+# #     return register_admin(data)
+
+# # @router.post("/admin/forgot-password")
+# # def forgot_password_route(data: ForgotPassword):
+# #     return forgot_password(data)
+
+# # @router.post("/admin/verify-otp")
+# # def verify_otp_route(data: VerifyOtp):
+# #     return verify_otp(data)
+
+# # @router.post("/admin/reset-password")
+# # def reset_password_route(data: ResetPassword):
+# #     return reset_password(data)
 # from fastapi import APIRouter
 # from app.controller.user_controller import (
 #     register_admin,
@@ -16,25 +53,33 @@
 
 # router = APIRouter(prefix="/api")
 
+
+# # =========================
+# # AUTH (ADMIN + EMPLOYEE)
+# # =========================
 # @router.post("/login")
 # def login_route(data: Login):
 #     return login(data)
 
-# @router.post("/admin/register")
-# def register_admin_route(data: RegisterAdmin):
-#     return register_admin(data)
-
-# @router.post("/admin/forgot-password")
+# @router.post("/forgot-password")
 # def forgot_password_route(data: ForgotPassword):
 #     return forgot_password(data)
 
-# @router.post("/admin/verify-otp")
+# @router.post("/verify-otp")
 # def verify_otp_route(data: VerifyOtp):
 #     return verify_otp(data)
 
-# @router.post("/admin/reset-password")
+# @router.post("/reset-password")
 # def reset_password_route(data: ResetPassword):
 #     return reset_password(data)
+
+
+# # =========================
+# # ADMIN ONLY
+# # =========================
+# @router.post("/admin/register")
+# def register_admin_route(data: RegisterAdmin):
+#     return register_admin(data)
 from fastapi import APIRouter
 from app.controller.user_controller import (
     register_admin,
